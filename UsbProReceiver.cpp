a4b8d1108cef999286118c0f4784064183fa32df
Simon Newton <nomis52@gmail.com>
2011-04-12 22:25:37 -0700

    * add the Usb Pro Receiver file

---
+++ UsbProReceiver.cpp
@@ -0,0 +1,72 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * UsbProReceiver.cpp
+ * Copyright (C) 2010 Simon Newton
+ */
+
+#include "UsbProReceiver.h"
+
+/*
+ * Read bytes from host
+ */
+void UsbProReceiver::Read() {
+  recieving_state recv_mode = PRE_SOM;
+  byte label = 0;
+  unsigned short expected_size = 0;
+  unsigned short data_offset = 0;
+  byte message[600];
+
+  while (Serial.available()) {
+    byte data = Serial.read();
+    switch (recv_mode) {
+      case PRE_SOM:
+        if (data == 0x7E) {
+          recv_mode = GOT_SOM;
+        }
+        return;
+      case GOT_SOM:
+        label = data;
+        recv_mode = GOT_LABEL;
+        return;
+      case GOT_LABEL:
+        data_offset = 0;
+        expected_size = data;
+        recv_mode = GOT_DATA_LSB;
+        return;
+      case GOT_DATA_LSB:
+        expected_size += (data << 8);
+        if (expected_size == 0) {
+          recv_mode = WAITING_FOR_EOM;
+        } else {
+          recv_mode = IN_DATA;
+        }
+        return;
+      case IN_DATA:
+        message[data_offset] = data;
+        data_offset++;
+        if (data_offset == expected_size) {
+          recv_mode = WAITING_FOR_EOM;
+        }
+        return;
+      case WAITING_FOR_EOM:
+        if (data == 0xE7) {
+          // this was a valid packet, act on it
+          TakeAction(label, message, expected_size);
+        }
+        recv_mode = PRE_SOM;
+    }
+  }
+}
---
+++ UsbProReceiver.h
@@ -0,0 +1,48 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * UsbProReceiver.h
+ * Copyright (C) 2011 Simon Newton
+ * A class which unpacks messages in the Usb Pro format.
+ */
+
+#ifndef USBPRO_RECEIVER_H_
+#define USBPRO_RECEIVER_H_
+
+/**
+ * Recieves a message over the serial link
+ */
+class UsbProReceiver {
+  public:
+    UsbProReceiver(
+      void (*callback)(byte label, byte *message, unsigned int size)):
+        m_callback(callback) {}
+    void Read();
+
+  private:
+    void (*m_callback)(byte label, byte *message, unsigned int size);
+
+    // The receiving state
+    typedef enum {
+      PRE_SOM = 0,
+      GOT_SOM = 1,
+      GOT_LABEL = 2,
+      GOT_DATA_LSB = 3,
+      IN_DATA = 4,
+      WAITING_FOR_EOM = 5,
+    } recieving_state;
+};
+
+#endif  // USBPRO_RECEIVER_H_

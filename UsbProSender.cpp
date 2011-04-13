6c7bcedfd1887786775d4f98e0023e96c39c6059
Simon Newton <nomis52@gmail.com>
2011-04-13 08:39:17 -0700

    * add the UsbProSender class

---
+++ UsbProSender.cpp
@@ -0,0 +1,51 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * UsbProSender.cpp
+ * Copyright (C) 2011 Simon Newton
+ */
+
+#include "UsbProSender.h"
+
+
+/**
+ * Sends the message header
+ */
+void UsbProSender::SendMessageHeader(byte label, int size) {
+  Serial.write(0x7E);
+  Serial.write(label);
+  Serial.write(size);
+  Serial.write(size >> 8);
+}
+
+/**
+ * Sends the message footer
+ */
+void UsbProSender::SendMessageFooter() {
+  Serial.write(0xE7);
+}
+
+
+/*
+ * Send a message to the host
+ * @param label the message label
+ * @param size the length of the data
+ * @param data the data buffer
+ */
+void UsbProSender::WriteMessage(byte label, int size, byte data[]) {
+  SendMessageHeader(label, size);
+  Serial.write(data, size);
+  SendMessageFooter();
+}
---
+++ UsbProSender.h
@@ -0,0 +1,39 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * UsbProSender.h
+ * Copyright (C) 2011 Simon Newton
+ */
+
+#include "WProgram.h"
+
+#ifndef USBPRO_SENDER_H
+#define USBPRO_SENDER_H
+
+/**
+ * Sends a properly framed message over the serial link
+ */
+class UsbProSender {
+  public:
+    UsbProSender() {}
+
+    void SendMessageHeader(byte label, int size);
+    void SendMessageFooter();
+
+    // helper message to send an array of bytes
+    void WriteMessage(byte label, int size, byte data[]);
+};
+
+#endif  // USBPRO_SENDER_H
